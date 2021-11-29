119273c2ba281cd52b3afdf0ad699c6568684611
Kyriacos Kyriacou <me@kkyr.io>
2021-11-29 23:33:38 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Advent of Code 2021
+
+🎄 [Advent of Code](https://adventofcode.com) is an Advent calendar of small programming puzzles. This year I'll be solving it in Python.
+
+## Running the code
+
+```
+$ python3 day-xx/main.py
+```
---
+++ day-xx/main.py
@@ -0,0 +1,12 @@
+def part_one(filename: str) -> int:
+    return 0
+
+def part_two(filename: str) -> int:
+    return 0
+
+if __name__ == "__main__":
+    input_path = "day-xx/input.txt"
+    print("Part 1")
+    print(part_one(input_path))
+    print("Part 2")
+    print(part_two(input_path))
