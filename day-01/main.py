2a1dc5944732259fa13bc8a60881311d22675a80
Kyriacos Kyriacou <me@kkyr.io>
2021-12-01 23:25:37 +0100

    Improve upon day-01

--- day-01/main.py
+++ day-01/main.py
@@ -1,22 +1,15 @@
-from typing import List
+#!/usr/bin/env python3
+def part_one(depths):
+    return sum(x < y for x, y in zip(depths, depths[1:]))
 
-def part_one(filename: str) -> int:
-    with open(filename) as f:
-        depths = list(map(int, f))
-    return num_increasing(depths)
-
-def part_two(filename: str) -> int:
-    with open(filename) as f:
-        depths = list(map(int, f))
-    window_depths = [sum([depths[j] for j in range(i, i+3)]) for i in range(len(depths)-2)]
-    return num_increasing(window_depths)
-
-def num_increasing(depths: List[str]) -> int:
-    return sum([1 if depths[i] > depths[i-1] else 0 for i in range(1, len(depths))])
+def part_two(depths):
+    return sum(x < y for x, y in zip(depths, depths[3:]))
         
 if __name__ == "__main__":
     input_path = "day-01/input.txt"
+    with open(input_path) as f:
+        depths = list(map(int, f))
     print("Part 1")
-    print(part_one(input_path))
+    print(part_one(depths))
     print("Part 2")
-    print(part_two(input_path))
+    print(part_two(depths))
--- day-xx/main.py
+++ day-xx/main.py
@@ -1,12 +1,15 @@
-def part_one(filename: str) -> int:
+#!/usr/bin/env python3
+def part_one(input):
     return 0
 
-def part_two(filename: str) -> int:
+def part_two(input):
     return 0
 
 if __name__ == "__main__":
-    input_path = "day-xx/input.txt"
+    input_path = "day-01/input.txt"
+    with open(input_path) as f:
+        input = list(map(int, f))
     print("Part 1")
-    print(part_one(input_path))
+    print(part_one(input))
     print("Part 2")
-    print(part_two(input_path))
+    print(part_two(input))
